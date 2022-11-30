4d7194767d147e71912429457dd8c1f370ff2a29
eriktamano <eriktamano@gmail.com>
2022-11-30 19:50:17 +0100

    TableLayout creat

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.sudoku;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TableLayout sudokuLayout = findViewById(R.id.sudokuLayout);
+        TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT);
+        for (int row = 0; row < 9; row++) {
+            TableRow fila = new TableRow(this);
+            fila.setLayoutParams(params);
+            for (int col = 0; col < 9; col++) {
+                Spinner spinner = new Spinner(this);
+                CharSequence[] nombres = {"0","1","2","3","4","5","6","7","8","9"};
+                ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
+                        android.R.layout.simple_spinner_item, nombres);
+                spinner.setAdapter(adapter);
+                spinner.setBackground(null);
+                spinner.setPadding(5, 5, 5, 5);
+                spinner.setLayoutParams(params);
+                fila.addView(spinner);
+            }
+            sudokuLayout.addView(fila);
+        }
     }
 }
