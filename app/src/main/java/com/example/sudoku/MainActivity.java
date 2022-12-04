7892f998bf013da63dfcad9ec23a09ddfbfb59e5
eriktamano <eriktamano@gmail.com>
2022-12-04 23:57:09 +0100

    Spinners funcionals, falta evitar la creació de sudokus insolventables

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sudoku;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,9 +13,8 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    Spinner[][] spinners = new Spinner[9][9];
+public class MainActivity extends AppCompatActivity{
+    private Spinner[][] matrix = new Spinner[9][9];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,56 +22,70 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TableLayout sudokuLayout = findViewById(R.id.sudokuLayout);
-        TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT);
-        for (int row = 0; row < 9; row++) {
-            TableRow fila = new TableRow(this);
-            fila.setLayoutParams(params);
+        SudokuModel.creaPartida();
+        for(int row = 0; row < 9; row++) {
+            TableRow tableRow = new TableRow(this);
             for (int col = 0; col < 9; col++) {
+                CharSequence[] numeros = {"0","1","2","3","4","5","6","7","8","9"};
                 Spinner spinner = new Spinner(this);
-                CharSequence[] nombres = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-                ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
-                        android.R.layout.simple_spinner_item, nombres);
-                spinner.setAdapter(adapter);
-                spinner.setBackground(null);
+                spinner.setBackground(getDrawable(R.drawable.border));
                 spinner.setTag(R.id.fila, row);
                 spinner.setTag(R.id.col, col);
-                spinner.setTag("init");
-                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                        if (spinner.getTag().equals("init")) {
-                            spinner.setTag("initn't");
-                        } else {
-                            SudokuModel.setVal((int) spinner.getTag(R.id.fila), (int) spinner.getTag(R.id.col), (int) spinner.getSelectedItem());
-                            refrescaGUI();
-                        }
-                    }
-
-                    @Override
-                    public void onNothingSelected(AdapterView<?> adapterView) {
-                    }
-                });
                 spinner.setPadding(5, 5, 5, 5);
-                spinner.setLayoutParams(params);
-                spinners[row][col] = spinner;
-                fila.addView(spinner);
+                spinner.setTag("init");
                 if (SudokuModel.getVal(row, col) != 0) {
                     spinner.setSelection(SudokuModel.getVal(row, col));
                     spinner.setEnabled(false);
                 }
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int b, long l) {
+                        if (adapterView.getTag().equals("init")) {
+                            adapterView.setTag("initn't");
+                        } else {
+                            if (!(adapterView.getSelectedItem().toString().equals("0"))) {
+                                int fila = (int) adapterView.getTag(R.id.fila);
+                                int columna = (int) adapterView.getTag(R.id.col);
+                                int valor = Integer.parseInt(adapterView.getSelectedItem().toString());
+
+                                if (SudokuModel.setVal(fila, columna, valor) == -1) {
+                                    Toast.makeText(getApplicationContext(), "T'has equivocat!!!", Toast.LENGTH_LONG).show();
+                                } else {
+                                    if (SudokuModel.checkCompleted()) {
+                                        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                                        alertDialog.setTitle("Sudoku");
+                                        alertDialog.setMessage("Enhorabona, has completat el sudoku!");
+
+                                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int l) {
+                                            }
+                                        });
+                                        alertDialog.show();
+                                    }
+                                }
+                            }
+                            refrescaGUI();
+                        }
+                    }
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+                ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, numeros);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(adapter);
+                matrix[row][col]=spinner;
+                tableRow.addView(spinner);
             }
-            sudokuLayout.addView(fila);
-
+            sudokuLayout.addView(tableRow);
         }
-        Toast.makeText(this, "iniciando", Toast.LENGTH_SHORT);
-        SudokuModel.creaPartida();
+        refrescaGUI();
     }
-
-    public void refrescaGUI() {
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                spinners[row][col].setSelection(SudokuModel.getVal(row, col));
-                System.out.println("Spinner canviat");
+    private void refrescaGUI(){
+        for(int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                matrix[i][j].setSelection(SudokuModel.getVal(i,j));
             }
         }
     }
--- app/src/main/java/com/example/sudoku/SudokuModel.java
+++ app/src/main/java/com/example/sudoku/SudokuModel.java
@@ -10,24 +10,24 @@ public class SudokuModel {
     }
 
     public static int setVal(int row, int col, int value) {
-        try {
-            matrix[row][col] = value;
-            if (value != 0 && (!comprovaFila(row) || !comprovaCol(col) || comprovaQuad(row, col))) {
-                return -1;
-            }
-            return value;
-        } catch (Exception e) {
+        int ogValue = getVal(row, col);
+        matrix[row][col] = value;
+        if (value != 0 && (!comprovaFila(row) || !comprovaCol(col) || !comprovaQuad(row, col))) {
+            matrix[row][col] = ogValue;
             return -1;
         }
+        return value;
     }
 
     public static boolean comprovaFila(int row) {
         ArrayList<Integer> vals = new ArrayList<Integer>();
         for (int col = 0; col < 9; col++) {
-            if (vals.contains(matrix[row][col])) {
-                return false;
+            if(matrix[row][col] != 0) {
+                if (vals.contains(matrix[row][col])) {
+                    return false;
+                }
+                vals.add(matrix[row][col]);
             }
-            vals.add(matrix[row][col]);
         }
         return true;
     }
@@ -35,47 +35,80 @@ public class SudokuModel {
     public static boolean comprovaCol(int col) {
         ArrayList<Integer> vals = new ArrayList<Integer>();
         for (int row = 0; row < 9; row++) {
-            if (vals.contains(matrix[row][col])) {
-                return false;
+            if(matrix[row][col] != 0) {
+                if (vals.contains(matrix[row][col])) {
+                    return false;
+                }
+                vals.add(matrix[row][col]);
             }
-            vals.add(matrix[row][col]);
         }
         return true;
     }
 
     public static boolean comprovaQuad(int row, int col) {
         ArrayList<Integer> vals = new ArrayList<Integer>();
-        int quad_row = row/3;
-        int quad_col = col/3;
-        for (int i = quad_row; i < quad_row + 3; i++) {
-            for (int j = quad_col; j < quad_col + 3; j++) {
-                if (vals.contains(matrix[i][j])) {
-                    return false;
+        int row_min;
+        int row_max;
+        int col_min;
+        int col_max;
+        if (row < 3) {
+            row_min = 0;
+            row_max = 3;
+        } else if (row < 6) {
+            row_min = 3;
+            row_max = 6;
+        } else {
+            row_min = 6;
+            row_max = 9;
+        }
+        if (col < 3) {
+            col_min = 0;
+            col_max = 3;
+        } else if (col < 6) {
+            col_min = 3;
+            col_max = 6;
+        } else {
+            col_min = 6;
+            col_max = 9;
+        }
+        for (int i = row_min; i < row_max; i++) {
+            for (int j = col_min; j < col_max; j++) {
+                if(matrix[i][j] != 0) {
+                    if (vals.contains(matrix[i][j])) {
+                        return false;
+                    }
+                    vals.add(matrix[i][j]);
                 }
-                vals.add(matrix[i][j]);
             }
         }
         return true;
     }
 
     public static void creaPartida() {
-        int fixedValues = (int) (Math.random() * 40) +1;
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 0; col++) {
-                if ((int) Math.random() == 1 && fixedValues > 0) {
-                    int correctVal = 0;
-                    while (true) {
-                        correctVal = setVal(row, col, (int) (Math.random() * 8) + 1);
-                        if (correctVal != -1) {
-                            break;
-                        }
-                    }
-                    fixedValues--;
-                } else {
-                    setVal(row, col, 0);
-                }
+        for (int row = 0; row < 9; row ++) {
+            for (int col = 0; col < 9; col++) {
+                setVal(row, col, 0);
+            }
+        }
+        int fixedValues = (int) (Math.random() * 20) + 30;
+        while (fixedValues > 0) {
+            int row = (int) (Math.random() * 9);
+            int col = (int) (Math.random() *9);
+            if (getVal(row, col) == 0) {
+                setVal(row, col, (int) (Math.random() * 8) + 1);
+                fixedValues--;
             }
         }
     }
 
+    public static boolean checkCompleted() {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (getVal(row, col) == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
